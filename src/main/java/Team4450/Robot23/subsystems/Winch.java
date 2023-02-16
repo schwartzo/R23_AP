a94ae9cf88ef9e1b441b511776f058d0a1451d69
Adam Palka <palkaap@students.osd.wednet.edu>
2023-02-15 17:04:22 -0800

    Removed CommandBase import

--- src/main/java/Team4450/Robot23/subsystems/Winch.java
+++ src/main/java/Team4450/Robot23/subsystems/Winch.java
@@ -2,7 +2,6 @@ package Team4450.Robot23.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static Team4450.Robot23.Constants.*;
