1034fb558eaa92cf621e4a26a423fd51bfed4c1a
Adam Palka <palkaap@students.osd.wednet.edu>
2023-02-16 16:40:05 -0800

    Added another constructor to AutoPositions. Made claw not accidently drop anything when using comboState

--- src/main/java/Team4450/Robot23/commands/AutoPositions.java
+++ src/main/java/Team4450/Robot23/commands/AutoPositions.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 public class AutoPositions extends CommandBase {
-    private boolean doingComboState = false;
+    private boolean doingComboState;
     private Arm arm;
     private Winch winch;
     private Claw claw;
@@ -21,10 +21,27 @@ public class AutoPositions extends CommandBase {
     private Command winchCommand;
     private Command clawCommand;
 
-    AutoPositions(Arm arm, Winch winch, Claw claw) {
+    AutoPositions(Arm arm, Winch winch, Claw claw, ComboStateNames comboState) {
+        doingComboState = true;
+
         this.arm = arm;
         this.winch = winch;
         this.claw = claw;
+
+        this.comboState = comboState;
+    }
+
+    AutoPositions(Arm arm, Winch winch, Claw claw, 
+                ArmStateNames armState, WinchStateNames winchState, ClawStateNames clawState) {
+        doingComboState = false;
+
+        this.arm = arm;
+        this.winch = winch;
+        this.claw = claw;
+
+        this.armState = armState;
+        this.winchState = winchState;
+        this.clawState = clawState;
     }
 
     // enum's for all the names of the different pre-set states.
@@ -37,10 +54,10 @@ public class AutoPositions extends CommandBase {
     }
 
     public enum ComboStateNames {
-        NONE, FULLY_CONTAINED, OBJECT_PICKUP, LOWEST_SCORING, MIDDLE_SCORING, HIGHEST_SCORING
+        FULLY_CONTAINED, OBJECT_PICKUP, LOWEST_SCORING, MIDDLE_SCORING, HIGHEST_SCORING
     }
 
-    public enum ClawStates {
+    public enum ClawStateNames {
         FULLY_OPEN, HOLDING_CUBE, HOLDING_CONE, CLOSED
     }
 
@@ -48,13 +65,13 @@ public class AutoPositions extends CommandBase {
     private ComboStateNames comboState;
     private ArmStateNames armState;
     private WinchStateNames winchState;
-    private ClawStates clawState;
+    private ClawStateNames clawState;
 
     // HashMap's containing the revolution amounts for certain states for the Arm,
     // Winch, and Claw
     private HashMap<ArmStateNames, Double> armStates = new HashMap<ArmStateNames, Double>();
     private HashMap<WinchStateNames, Double> winchStates = new HashMap<WinchStateNames, Double>();
-    private HashMap<ClawStates, Double> clawStates = new HashMap<ClawStates, Double>();
+    private HashMap<ClawStateNames, Double> clawStates = new HashMap<ClawStateNames, Double>();
 
     public void initialize() {
         // Creating inital states, where the doubles are target revolutions
@@ -75,17 +92,18 @@ public class AutoPositions extends CommandBase {
         winchStates.put(WinchStateNames.MAX_HEIGHT, 0.0);
 
         // clawStates
-        clawStates.put(ClawStates.FULLY_OPEN, 0.0);
-        clawStates.put(ClawStates.HOLDING_CUBE, 0.0);
-        clawStates.put(ClawStates.HOLDING_CONE, 0.0);
-        clawStates.put(ClawStates.CLOSED, 0.0);
+        clawStates.put(ClawStateNames.FULLY_OPEN, 0.0);
+        clawStates.put(ClawStateNames.HOLDING_CUBE, 0.0);
+        clawStates.put(ClawStateNames.HOLDING_CONE, 0.0);
+        clawStates.put(ClawStateNames.CLOSED, 0.0);
 
         commands = new ParallelCommandGroup();
 
         // Sets the commands
         armCommand = new AutoArm(arm, (doingComboState) ? armStates.get(comboState) : armStates.get(armState), 0);
         winchCommand = new AutoWinch(winch, (doingComboState) ? winchStates.get(comboState) : winchStates.get(winchState), 0);
-        clawCommand = new AutoClaw(claw, (doingComboState) ? armStates.get(comboState) : clawStates.get(clawState), 0);
+        if (comboState == ComboStateNames.OBJECT_PICKUP)
+            clawCommand = new AutoClaw(claw, clawStates.get(ClawStateNames.FULLY_OPEN), 0);
 
         // Adds the commands
         commands.addCommands(armCommand, winchCommand, clawCommand);
