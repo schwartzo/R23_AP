2029b6caf5d459303078c8461b5ed7b2aaca8700
schwartzo <schwartzo@students.osd.wednet.edu>
2023-03-13 18:23:31 -0700

    Start work on pathfinding

---
+++ src/main/java/Team4450/Robot23/pathfinder/FieldMap.java
@@ -0,0 +1,12 @@
+package Team4450.Robot23.pathfinder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FieldMap {
+    private List<Obstacle> obstacles = new ArrayList<>();
+
+    public static abstract class Obstacle {
+
+    }
+}
